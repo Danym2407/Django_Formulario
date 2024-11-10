41d4b5e9f4072d856f35c3e08796cd7391403d20
root <root@Donydonitass.>
2024-11-09 21:06:05 -0600

    Commit 3 - Creamos nuestra aplicación main para inciar con nuestro formulario

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_Formulario1.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_form1.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
